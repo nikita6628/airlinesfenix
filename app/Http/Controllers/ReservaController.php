7bbf20231365b8ba633c4d85425c56cd3cf62350
Mauro Rambo <mauroemmanuel22@gmail.com>
2021-03-20 23:15:31 -0300

    lista de reservas por usuario

--- app/Http/Controllers/ReservaController.php
+++ app/Http/Controllers/ReservaController.php
@@ -3,7 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Reserva;
+use App\Models\Vuelo;
+use App\Models\Cabina;
+use App\Models\Estado;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Auth;
+
 
 class ReservaController extends Controller
 {
@@ -19,8 +25,18 @@ class ReservaController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        return view('home');
+    { 
+        $id = Auth::user()->id;  
+        
+        $reservas = DB::table('reservas')
+            ->join('vuelos', 'reservas.vuelo_id', '=', 'vuelos.id')
+            ->join('lugars', 'vuelos.origen_id', '=', 'lugars.id')
+            ->join('estados', 'vuelos.estado_id', '=', 'estados.id')
+            ->select('reservas.id','vuelos.nro_vuelo','vuelos.fecha_salida','lugars.ciudad','estados.tipo')
+            ->where('user_id',$id)
+            ->get();
+        //var_dump($reservas);
+        return view('home')->with('reservas', $reservas);
     }
 
     /**
@@ -30,7 +46,7 @@ class ReservaController extends Controller
      */
     public function create()
     {
-        //
+        return view('reservas.nueva');
     }
 
     /**
@@ -52,7 +68,16 @@ class ReservaController extends Controller
      */
     public function show(Reserva $reserva)
     {
-        //
+        /*$id = Auth::user()->id;  
+        $reservas = DB::table('reservas')
+            ->join('vuelos', 'reservas.vuelo_id', '=', 'vuelo.id')
+            ->join('cabinas', 'reservas.cabina_id', '=', 'cabinas.id')
+            ->join('estados', 'vuelos.estado_id', '=', 'estados.id')
+            ->select('reservas.tarifa', 'reservas.asiento', 'vuelo.nro_vuelo')
+            ->where('user_id',$id)
+            ->get();
+        return view('home')->with('reservas', $reservas);*/
+        return view('reservas.detalle');
     }
 
     /**
@@ -63,7 +88,7 @@ class ReservaController extends Controller
      */
     public function edit(Reserva $reserva)
     {
-        //
+        return view('reservas.editar');
     }
 
     /**
---
+++ resources/views/reservas/editar.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <h1>Editar Reserva</h1>   
+        </div>
+    </div>
+</div>
+@endsection
